b338268cfe360b8b318110786caf74e577b2d5c0
Simone Westphal <contact@swestphal.net>
2018-03-03 16:51:54 +0100

    feat: add main eventlisteners to controller

--- app.js
+++ app.js
@@ -5,7 +5,20 @@ var budgetController = (function() {})();
 var uiController = (function() {})();
 
 // global controller
-var controller = (function(budgetCtrl, uiCtrl) {})(
-    budgetController,
-    uiController
-);
+var controller = (function(budgetCtrl, uiCtrl) {
+    var ctrlAddItem = function() {
+        // get field input
+        // add item to the budget controller
+        // add new item to ui
+        // calculate budget
+        // display budget
+    };
+
+    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function(e) {
+        if (e.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+})(budgetController, uiController);
