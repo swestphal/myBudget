5908e1a5fac3013a56f0422525d18939073b2dc9
Simone Westphal <contact@swestphal.net>
2018-03-03 16:29:19 +0100

    feat: set up controller

---
+++ app.js
@@ -0,0 +1,11 @@
+// data controller
+var budgetController = (function() {})();
+
+// user interface controller
+var uiController = (function() {})();
+
+// global controller
+var controller = (function(budgetCtrl, uiCtrl) {})(
+    budgetController,
+    uiController
+);
