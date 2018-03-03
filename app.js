5501f2afd8581b361883db2e68fc3d1f4be37206
Simone Westphal <contact@swestphal.net>
2018-03-03 17:13:54 +0100

    feat: set up ui controller

--- app.js
+++ app.js
@@ -2,19 +2,44 @@
 var budgetController = (function() {})();
 
 // user interface controller
-var uiController = (function() {})();
+var uiController = (function() {
+    var domStr = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputBtn: ".add__btn"
+    };
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(domStr.inputType).value,
+                description: document.querySelector(domStr.inputDescription)
+                    .value,
+                value: document.querySelector(domStr.inputValue).value
+            };
+        },
+        getDomStr: function() {
+            return domStr;
+        }
+    };
+})();
 
 // global controller
 var controller = (function(budgetCtrl, uiCtrl) {
+    var dom = uiController.getDomStr();
+
     var ctrlAddItem = function() {
         // get field input
+        var input = uiCtrl.getInput();
+        console.log(input);
         // add item to the budget controller
         // add new item to ui
         // calculate budget
         // display budget
     };
 
-    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+    document.querySelector(dom.inputBtn).addEventListener("click", ctrlAddItem);
 
     document.addEventListener("keypress", function(e) {
         if (e.keyCode === 13 || event.which === 13) {
